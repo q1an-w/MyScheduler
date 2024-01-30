5481a551bcfd1bae9dda33805bd8ad294b28b42f
q1an-w <qianywang25@gmail.com>
2024-01-29 22:01:45 -0500

    ts next app

---
+++ ReactFrontend/webapp/src/app/home/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+    return (
+      <main >
+        <div style={{marginLeft:"500px", marginTop:"500px"}}>
+          adfgsdfgsdfgsdfg
+        </div>
+      </main>
+    )
+  }
